4de8987ef8d057195ee5f5eb00e070844ce751db
Riley Gough 759155 <759155@newcollege.ac.uk>
2026-01-15 12:59:36 +0000

    g

--- Assets/Scripts/Player/PlayerAttackHitbox.cs
+++ Assets/Scripts/Player/PlayerAttackHitbox.cs
@@ -4,7 +4,7 @@ public class PlayerAttackHitbox : MonoBehaviour
 {
     public PlayerController2D player;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (!player.isAttacking)
             return;
